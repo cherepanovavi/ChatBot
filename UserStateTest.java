bcc787f83a95060e6b5576b4701bec0e4b5befbb
Unknown <cherepanovaviktoria@gmail.com>
2018-10-24 10:57:39 +0500

    Добавлены тесты к классу пользователя

--- UserStateTest.java
+++ UserStateTest.java
@@ -23,6 +23,27 @@ class UserStateTest {
 		int questNum = user.getQuestionNumber();
 		user.moveToNextQuestion();
 		assertEquals(user.getQuestionNumber(), questNum+1);
+		assertEquals(user.getQuestionAttempts(), ChatBot.attemptsCount);
+	}
+
+	@Test
+	void spendAttempt()
+	{
+		UserState user = new UserState("Афанасий");
+		user.spendAnAttempt();
+		assertEquals(user.getQuestionNumber(), 0);
+		assertEquals(user.getQuestionAttempts(), ChatBot.attemptsCount -1);
+
+	}
+
+	@Test
+	void addScores()
+	{
+		UserState user = new UserState("Афанасий");
+		int scores1= user.getScore();
+		user.addScores(2);
+		assertEquals(scores1 + 2, user.getScore());
+
 	}
 
 
